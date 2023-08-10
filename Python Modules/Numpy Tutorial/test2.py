daa80d42436466b78c59489325c2554add088645
Jay Wijesekera <139295545+jayw20230711@users.noreply.github.com>
2023-08-10 15:25:28 -0400

    Add files via upload

---
+++ Python Modules/Numpy Tutorial/test2.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+# sns.displot(([0, 1, 2, 3, 4, 5]))
+
+# plt.show()
+
+x = np.array([9, 10,  11])
+y = x[0:1]
+print(y)
+y[0] = 15
+
+print(y)
+print(x)
