7cbde7cda589bfd7dd8af7a71fae6a2a49213a64
Jay Wijesekera <139295545+jayw20230711@users.noreply.github.com>
2023-08-10 14:54:05 -0400

    Add files via upload

---
+++ Introduction to Python/3. Python Syntax.py
@@ -0,0 +1,61 @@
+ """
+Python Syntax
+
+Execute Python Syntax
+Python Indentation
+Python Variables
+Python Comments
+Exercises
+
+
+Execute Python Syntax
+As we learned in the previous page, Python syntax can be executed by writing directly in the Command Line:
+
+# >>> print("Hello, World!")
+Hello, World!
+
+
+
+Python Indentation
+Indentation refers to the spaces at the beginning of a code line.
+
+Where in other programming languages the indentation in code is for readability only, the indentation in Python is
+very important.
+
+Python uses indentation to indicate a block of code.
+
+"""
+
+if 5 > 2:
+    print("Five is greater than two!")
+
+"""
+Python will give you an error if you skip the indentation:
+"""
+# if 5 > 2:
+# print("Five is greater than two!")
+
+"""
+The number of spaces is up to you as a programmer, the most common use is four, but it has to be at least one.
+
+You have to use the same number of spaces in the same block of code, otherwise Python will give you an error:
+
+if 5 > 2:
+ print("Five is greater than two!")
+        print("Five is greater than two!")
+
+
+"""
+
+"""
+Python Variables
+In Python, variables are created when you assign a value to it:
+
+Example
+Variables in Python:
+
+x = 5
+y = "Hello, World!"
+
+
+"""
