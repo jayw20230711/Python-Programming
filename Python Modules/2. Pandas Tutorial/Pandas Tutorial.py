e36fee02b5f956df5f59f1bf15f148707b67e35a
Jay Wijesekera <139295545+jayw20230711@users.noreply.github.com>
2023-08-11 21:35:22 -0400

    Add files via upload

---
+++ Python Modules/2. Pandas Tutorial/Pandas Tutorial.py
@@ -0,0 +1,34 @@
+"""
+Pandas Tutorial
+
+Pandas is a Python library.
+
+Pandas is used to analyze data.
+
+Learning by Reading
+We have created 14 tutorial pages for you to learn more about Pandas.
+
+Starting with a basic introduction and ends up with cleaning and plotting data:
+
+BASIC
+Introduction
+Getting Started
+Pandas Series
+DataFrames
+Read CSV
+Read JSON
+Analyse Data
+
+CLEANING DATA
+Clean Data
+Clean Empty Cells
+Clean Wrong Format
+Clean Wrong Data
+Remove Duplicates
+
+ADVANCED
+Correlations
+Plotting
+
+
+"""
